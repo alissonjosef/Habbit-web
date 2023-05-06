998713291716238b7866a9317e4aa3d7b5010ef4
Alisson Silva <alissonsilva@MacBook-Pro-de-Alisson.local>
2023-05-06 00:30:20 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Header } from './components/Header'
+import { SummanyTable } from './components/SummanyTable'
+import './styles/global.css'
+import { Plus } from 'phosphor-react'
+//import { Habit } from './components/Habit'
+
+export function App() {
+
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+      <Header/>
+      <SummanyTable />
+      </div>
+    </div>
+  )
+}
+
